9ce39bfc3cfd6f0dc8b0583c263debc2455cc2d4
Aaron Tagadtad <aaron@alias.com>
2022-05-12 12:42:30 -0400

    .js

--- dist/graphql-schema-modules.mjs
+++ dist/graphql-schema-modules.mjs
@@ -1,25 +1,22 @@
-import gql from 'graphql-tag';
-import { print } from 'graphql/language/printer';
-import deepExtend from 'deep-extend';
-import requireAll from 'require-all';
-import shim from 'core-js/fn/object/values';
+import gql from "graphql-tag";
+import { print } from "graphql/language/printer.js";
+import deepExtend from "deep-extend";
+import requireAll from "require-all";
+import shim from "core-js/fn/object/values";
 export function mergeModules(modules = []) {
-  const typeDefs = mergeTypeDefs(modules.filter(({
-    typeDefs
-  }) => !!typeDefs).map(({
-    typeDefs
-  }) => typeDefs));
-  const resolvers = mergeResolvers(modules.filter(({
-    resolvers
-  }) => !!resolvers).map(({
-    resolvers
-  }) => resolvers));
+  const typeDefs = mergeTypeDefs(
+    modules.filter(({ typeDefs }) => !!typeDefs).map(({ typeDefs }) => typeDefs)
+  );
+  const resolvers = mergeResolvers(
+    modules
+      .filter(({ resolvers }) => !!resolvers)
+      .map(({ resolvers }) => resolvers)
+  );
   return {
     typeDefs: [typeDefs],
-    resolvers
+    resolvers,
   };
 }
-;
 export function loadModules(directory) {
   const modules = [];
   requireAll({
@@ -28,34 +25,38 @@ export function loadModules(directory) {
 
     resolve(mod) {
       modules.push(mod);
-    }
-
+    },
   });
   return mergeModules(modules);
 }
-;
-
 const typeDefsToStr = (typeDefs = []) => {
-  if (typeof typeDefs === 'string') {
+  if (typeof typeDefs === "string") {
     return typeDefs;
-  } else if (typeof typeDefs === 'function') {
+  } else if (typeof typeDefs === "function") {
     return typeDefsToStr(typeDefs());
   } else {
-    return typeDefs.map(typeDef => typeDefsToStr(typeDef)).join('\n');
+    return typeDefs.map((typeDef) => typeDefsToStr(typeDef)).join("\n");
   }
 };
 
 const mergeTypeDefs = (typeDefs = []) => {
-  typeDefs = typeDefs.map(typeDefs => gql`${typeDefsToStr(typeDefs)}`);
+  typeDefs = typeDefs.map(
+    (typeDefs) =>
+      gql`
+        ${typeDefsToStr(typeDefs)}
+      `
+  );
   const rootTypes = {};
-  typeDefs.forEach(graphql => {
-    graphql.definitions.forEach(def => {
+  typeDefs.forEach((graphql) => {
+    graphql.definitions.forEach((def) => {
       const name = def.name.value;
 
       if (rootTypes[name]) {
         // throw error if enum with same name is defined twice
-        if (rootTypes[name].kind === 'EnumTypeDefinition') {
-          throw new Error(`duplicate enum definition '${rootTypes[name].name.value}'`);
+        if (rootTypes[name].kind === "EnumTypeDefinition") {
+          throw new Error(
+            `duplicate enum definition '${rootTypes[name].name.value}'`
+          );
         }
 
         rootTypes[name].fields = rootTypes[name].fields.concat(def.fields);
@@ -65,14 +66,14 @@ const mergeTypeDefs = (typeDefs = []) => {
     });
   });
   return print({
-    kind: 'Document',
-    definitions: Object.values(rootTypes)
+    kind: "Document",
+    definitions: Object.values(rootTypes),
   });
 };
 
 const mergeResolvers = (resolvers = []) => {
   const rootResolvers = {};
-  resolvers.forEach(_resolvers => {
+  resolvers.forEach((_resolvers) => {
     deepExtend(rootResolvers, _resolvers);
   });
   return rootResolvers;
